b0488a249e69230df9ad81bd05b05bf5b6261b7b
Renato Silvestre (SilvestreHH) <renatao.silvestre@gmail.com>
2019-06-28 16:59:15 -0300

    refactor do reset-password e adição de nova funcao de remove no mixin que deleta registros

--- src/pages/mixins/crud/delete-by-id.js
+++ src/pages/mixins/crud/delete-by-id.js
@@ -7,7 +7,7 @@ export const deleteById = {
     block (payload) {
       this.$q.dialog({
         title: 'Confirmação',
-        message: 'Bloquear Usuário ?',
+        message: 'Bloquear ?',
         ok: 'SIM',
         cancel: 'NÃO'
       }).onOk(() => {
@@ -18,6 +18,28 @@ export const deleteById = {
           this.fetch({ pagination: this.serverPagination })
           this.setFetching({ fetching: false })
           this.setMessage({ type: 'warning', message: 'Bloqueado' })
+        })
+          .catch((error) => {
+            console.log(error)
+            this.setFetching({ fetching: false })
+          })
+      }).onCancel(() => {
+      })
+    },
+    remove (payload) {
+      this.$q.dialog({
+        title: 'Confirmação',
+        message: 'Excluir ?',
+        ok: 'SIM',
+        cancel: 'NÃO'
+      }).onOk(() => {
+        const token = currentToken()
+        const id = payload.selected._id
+        this.setFetching({ fetching: true })
+        this.services.deleteById(this.model, id, token).then(() => {
+          this.fetch({ pagination: this.serverPagination })
+          this.setFetching({ fetching: false })
+          this.setMessage({ type: 'warning', message: 'Excluido' })
         })
           .catch((error) => {
             console.log(error)
